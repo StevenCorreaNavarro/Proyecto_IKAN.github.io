4ee70e6cdeaacdbb4111df47694a097dd32ca860
LuisIrua23 <lairua@soy.sena.edu.co>
2024-04-14 14:13:40 -0500

    luis

---
+++ PRUEBAS/Login-y-registro-con-html-css-js-main/Login-y-registro-con-html-css-js-main/assets/js2.js
@@ -0,0 +1,8 @@
+
+document.getElementById('btn__iniciar-sesion').addEventListener('click', function() {
+    document.querySelector('.contenedor__login-register').style.display = 'block';
+});
+
+document.getElementById('btn__cerrar').addEventListener('click', function() {
+    document.querySelector('.contenedor__login-register').style.display = 'none';
+});
