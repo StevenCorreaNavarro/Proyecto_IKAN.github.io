8ce66344d2dc21aa0a42cdf955224c347d46bb55
LuisIrua23 <lairua@soy.sena.edu.co>
2024-04-17 23:14:24 -0500

    piru

--- 1.Home_login.js
+++ 1.Home_login.js
@@ -21,15 +21,6 @@ const Open2Btn = document.querySelector("#form-open2"),
             }
         });
     });
-    
-
-
-
-
-
-
-
-
 const formOpenBtn = document.querySelector("#form-open"),
     home = document.querySelector(".home"),             
     formContainer = document.querySelector(".form_container"),
